05683a1c111cfca57b2ac1d285881f0535e3a4d0
Erik Assum <erik.assum@ardoq.com>
2022-03-25 14:28:58 +0100

    We can listen to the zombiecljs websocket!

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { WebsocketBuilder } from "websocket-ts";
+
+const ws = new WebsocketBuilder("ws://localhost:8666/ws")
+  .onOpen((i, ev) => {
+    console.log("opened");
+  })
+  .onClose((i, ev) => {
+    console.log("closed");
+  })
+  .onError((i, ev) => {
+    console.log("error");
+  })
+  .onMessage((i, ev) => {
+    console.log("message", ev, i);
+  })
+  .onRetry((i, ev) => {
+    console.log("retry");
+  })
+  .build();
 
 function App() {
   return (
