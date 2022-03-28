35c699987736a6d6d7987a606bdc2f505a2f35c4
Erik Assum <erik.assum@ardoq.com>
2022-03-28 07:50:57 +0200

    Handle the initial value correctly

--- src/index.tsx
+++ src/index.tsx
@@ -10,25 +10,26 @@ import * as bus from "./bus";
 const websocket$ = webSocket("ws://localhost:8666/ws");
 const container = document.getElementById("root");
 
-const action$ = new Subject<Action[]>();
 const error = (msg: string) => (e: any) => console.log(`${msg}: ERROR`, e);
 const complete = (msg: string) => () => console.log(`${msg}: Completed`);
+
+const render = (s: Store) => ReactDOM.render(<Page {...s} />, container);
+
+const action$ = new Subject<Action[]>();
 
 websocket$.subscribe(
   (event) => {
     const json = JSON.parse(event as string);
     if (Array.isArray(json)) {
       action$.next(json);
+    } else {
+      action$
+        .pipe(scan(performActions, json))
+        .subscribe(render, error("STORE"), complete("STORE"));
     }
   },
   error("WS"),
   complete("WS")
 );
 
-const store$ = action$.pipe(scan(performActions, {}));
-
 bus.watch("me", Topic.PERFORM_ACTION, (actions) => action$.next(actions));
-
-const render = (s: Store) => ReactDOM.render(<Page {...s} />, container);
-
-store$.subscribe(render, error("STORE"), complete("STORE"));
